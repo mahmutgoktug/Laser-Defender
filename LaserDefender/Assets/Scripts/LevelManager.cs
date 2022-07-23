90f2ef68c18ba0db8f4b93bc3b3f6a28b65aea18
Mahmut Göktuğ <101980878+mahmutgoktug@users.noreply.github.com>
2022-07-23 20:55:49 +0300

    fix.

--- LaserDefender/Assets/Scripts/LevelManager.cs
+++ LaserDefender/Assets/Scripts/LevelManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    [SerializeField] float sceneLoadDelay = 2f;
+
     public void LoadGame()
     {
         SceneManager.LoadScene("Game");
@@ -17,12 +19,18 @@ public class LevelManager : MonoBehaviour
 
     public void LoadGameOver()
     {
-        SceneManager.LoadScene("GameOver");
+        StartCoroutine(WaitAndLoad("GameOver", sceneLoadDelay));
     }
 
     public void QuitGame()
     {
         Debug.Log("Quitting Game...");
         Application.Quit();
+    }
+
+    IEnumerator WaitAndLoad(string sceneName, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);
     }
 }
